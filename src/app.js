d77bbad2e6bf40b5b9974379baddf0c7ad010619
Ernesto Caraballo <ernestojcaraballog@gmail.com>
2025-06-17 18:14:50 +0000

    Cleaned the code for the excuse generator

--- src/app.js
+++ src/app.js
@@ -4,26 +4,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 // Arreglos con partes de la excusa
-let who = ['Darth Vader', 'Big Brother', 'The Shadow Government', 'My Reflection', 'John Wick', 'The Bank'];
-let action = ['ate', 'disassembled', 'crushed', 'broke', 'disappeared', 'helped me with'];
-let what = ['my homework', 'my phone', 'the car', 'my family', 'my bank account', 'my savings'];
+let who = ['Darth Vader', 'Big Brother', 'The Shadow Government', 'My shadow', 'John Wick', 'The bank', 'The Pope', 'The Rebel Alliance'];
+let action = ['ate', 'disassembled', 'crushed', 'broke', 'disappeared', 'helped me with', 'liberated'];
+let what = ['my homework', 'my phone', 'the car', 'my family', 'my bank account', 'my savings', 'the US government', 'the moon',];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during lunch', 'while I was praying'];
 
+const pickRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
 
 function excuseGenerator() {
-  let excuse = "";
-  excuse += who[Math.floor(Math.random() * who.length)] + " ";
-  excuse += action[Math.floor(Math.random() * action.length)] + " ";
-  excuse += what[Math.floor(Math.random() * what.length)] + " ";
-  excuse += when[Math.floor(Math.random() * when.length)];
-  return excuse;
+  return `${pickRandomElement(who)} ${pickRandomElement(action)} ${pickRandomElement(what)} ${pickRandomElement(when)}`;
 }
-
 
 function updateExcuse() {
-  document.getElementById("excuse").innerHTML = excuseGenerator();
+  document.getElementById("excuse").textContent = excuseGenerator();
 }
-
 
 window.onload = function () {
   updateExcuse();
